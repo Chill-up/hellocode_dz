ddcd3595cf8355aee6b0c19876252dbbf43111c7
Andrei <apchilap@gmail.com>
2022-08-05 11:25:35 +0300

    add task8 description in readme.md + task 8 code

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Все четные числа от 1 до {number} это: ");
+int even = 1;
+while (even <= number)
+{
+    if (even % 2 == 0)
+    {
+        Console.Write(even + " ");
+        even = even + 1;
+    }
+    else
+    {
+        even = even + 1;
+    }
+}
