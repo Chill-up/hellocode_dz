330da71ff1270cf9a91cb86312e951450e3179d2
Andrei <apchilap@gmail.com>
2022-08-09 17:36:44 +0300

    add code to task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -3,3 +3,27 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+int ShowSecondDigit(int num)
+{
+    int res = (num / 10) % 10;
+    if (res < 0) //фикс для отрицательных чисел
+    {
+        res = res * -1;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Вы ввели НЕ трехзначное число. Прощайте.");
+}
+else
+{
+    int secondDigit = ShowSecondDigit(number);
+    Console.WriteLine($"Вторая цифра введенного числа {number} -> {secondDigit} ");
+}
+
+
