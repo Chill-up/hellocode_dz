f2e1879b42f4140ffcb5e579af06473b208b78bf
Andrei <apchilap@gmail.com>
2022-09-02 23:22:33 +0300

    add task 54 code and fix readme

--- task054/Program.cs
+++ task054/Program.cs
@@ -7,3 +7,75 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+
+//     Console.WriteLine($"замена элемента {matrix[i, i]} на {matrix[i, i + 1]} ");
+//     int temp = matrix[i, i + 1]; //эта часть меняет элементы местами.
+//     matrix[i, i + 1] = matrix[i, i + 1];
+//     matrix[i, i + 1] = temp;
+// }
+
+void PrintMatrixInt(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" ]");
+    }
+}
+
+int[,] CreateMatrixRandomInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void SortMatrixRowNumbersMaxToMin(int[,] matrix)
+{
+    int rowLength = matrix.GetLength(0);
+    int colLength = matrix.GetLength(1);
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength - 1; j++)
+        {
+            int maxIndex = j; // индекс максимального элемента в строке
+            for (int k = j + 1; k < colLength; k++)  //j+1 чтобы не перебирать уже отсортированные элементы 
+            {
+                if (matrix[i, k] > matrix[i, maxIndex]) maxIndex = k;
+            }
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[i, maxIndex];
+            matrix[i, maxIndex] = temp;
+        }
+    }
+}
+
+Console.Clear();
+
+int numberRows = new Random().Next(2, 6); // генерируем размер массива
+int numberCols = new Random().Next(2, 12);
+
+int[,] randomMatrix = CreateMatrixRandomInt(numberRows, numberCols, 0, 100); // генерируем массив с которым будем работать.
+
+Console.WriteLine($"Сгенерирован следующий массив {numberRows} х {numberCols} : ");
+
+PrintMatrixInt(randomMatrix);
+
+SortMatrixRowNumbersMaxToMin(randomMatrix);
+
+Console.WriteLine();
+
+PrintMatrixInt(randomMatrix);
