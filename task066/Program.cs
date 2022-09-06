a9364d9341bc549dd2fba2c31e7fb9577c06d669
Andrei <apchilap@gmail.com>
2022-09-06 23:17:49 +0300

    add task 66 code

--- task066/Program.cs
+++ task066/Program.cs
@@ -3,3 +3,20 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+// m и n даны включительно.
+
+Console.WriteLine("Введите первое целое число:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int FindSumBetweenTwoIntNUmbers(int m, int n)
+{
+    int sum = m;
+    if (m < n - 1) return sum += FindSumBetweenTwoIntNUmbers(m + 1, n);
+    if (m > n + 1) return sum += FindSumBetweenTwoIntNUmbers(m - 1, n);
+    else return m + n;
+}
+
+int result = FindSumBetweenTwoIntNUmbers(numberM, numberN);
+Console.WriteLine($"Сумма чисел от M = {numberM} до N = {numberN} (включительно) -> {result}");
